9b1c96f7357f7086d23c70e0091b22fafffce936
nimamze <nimamze3@gmail.com>
2026-05-01 15:49:09 +0330

    add accounts utils jwt

--- Gateway/accounts/utils/jwt_blacklist.py
+++ Gateway/accounts/utils/jwt_blacklist.py
@@ -1,5 +1,9 @@
 from django.core.cache import cache
 import time
+from rest_framework_simplejwt.token_blacklist.models import (
+    BlacklistedToken,
+    OutstandingToken,
+)
 
 
 def blacklist_access_token(jti: str, exp: int):
@@ -10,3 +14,8 @@ def blacklist_access_token(jti: str, exp: int):
 
 def is_access_token_blacklisted(jti: str):
     return cache.get(f"access_blacklist:{jti}") is not None
+
+
+def blacklist_user_refresh_tokens(user):
+    for token in OutstandingToken.objects.filter(user=user):
+        BlacklistedToken.objects.get_or_create(token=token)
