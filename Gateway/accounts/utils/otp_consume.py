3765a724d5a34a0c80e9baa4babd750a71df4d08
nimamze <nimamze3@gmail.com>
2026-05-01 15:49:18 +0330

    add accounts utils otp consume

--- Gateway/accounts/utils/otp_consume.py
+++ Gateway/accounts/utils/otp_consume.py
@@ -1,14 +1,13 @@
 from django.core.cache import cache
 
 
-def consume_otp_authorization(user_phone, purpose):
-    limit_key = f"otp_consume_limit:{purpose}:{user_phone}"
+def consume_otp_authorization(target, purpose):
+    limit_key = f"otp_consume_limit:{purpose}:{target}"
+    cache.add(limit_key, 0, timeout=20)
     attempts = cache.incr(limit_key)
-    if attempts == 1:
-        cache.expire(limit_key, 20)  # type: ignore
     if attempts > 5:
         return False
-    key = f"can_{purpose}:{user_phone}"
+    key = f"can_{purpose}:{target}"
     result = cache.get(key)
     if result:
         cache.delete(key)
