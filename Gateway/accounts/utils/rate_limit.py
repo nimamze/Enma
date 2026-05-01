691e99e5878c13e3ef4723000f5dfa3a5be60e98
nimamze <nimamze3@gmail.com>
2026-05-01 15:49:31 +0330

    add accounts utils rate limit

--- Gateway/accounts/utils/rate_limit.py
+++ Gateway/accounts/utils/rate_limit.py
@@ -3,8 +3,7 @@ from rest_framework import serializers
 
 
 def atomic_rate_limit(key: str, ttl: int, max_attempts: int):
+    cache.add(key, 0, timeout=ttl)
     attempts = cache.incr(key)
-    if attempts == 1:
-        cache.expire(key, ttl)  # type: ignore
     if attempts > max_attempts:
         raise serializers.ValidationError("rate limit exceeded")
