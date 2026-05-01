6fa9f25695d11c7ba186ab9e1281e9abbba52396
nimamze <nimamze3@gmail.com>
2026-05-01 11:11:29 +0330

    move celery files

--- Gateway/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from Project.Enma.Gateway.celery import Celery
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
-app = Celery("django_celery")  # type: ignore
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
